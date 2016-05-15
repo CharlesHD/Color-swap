a44d45430b3c892a2964b6c86c70710cb62584a9
Charles HD <charleshdespointes@gmail.com>
2016-05-16 01:37:54 +0200

    added the entry point

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
 import Lib
+import System.Environment
 
 main :: IO ()
-main = someFunc
+main = do args <- getArgs
+          if length args > 1
+          then swapColorThem (head args) (args !! 1)
+          else swapColorThem "swap.bmp" "."
